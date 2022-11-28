aa1bef0e06381514e40707b378e3ee2c661c7d7a
SLys <1826841@crosemont.qc.ca>
2022-11-27 20:26:43 -0500

    Ajout du DataProvider & de son controller pour serialiser les enregistrements de la base de donnees en format gson.

---
+++ Defi_Lecture_API/Models/Lecture/Lecture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Defi_Lecture_API.Models.Lecture
+{
+    [DataContract]
+    public class Lecture
+    {
+        [DataMember]
+        public int idLecture { get; set; }
+        [DataMember]
+        public int idCompte { get; set; }
+        [DataMember]
+        public string titre { get; set; }
+        [DataMember]
+        public string dateInscription { get; set; }
+        [DataMember]
+        public int tempsLu { get; set; }
+        [DataMember]
+        public bool estObligatoire { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format(" Lecture #{} : Titre: {}, Compte: {}, Date: {}, Temps lu : {}, Obligatoire : {} ",
+                idLecture, titre, idCompte, dateInscription, tempsLu, estObligatoire);
+        }
+    }
+}
